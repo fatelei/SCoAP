f0911c431cdaa4eb20d8b5f54ba76d958550a60b
wanglei <fatelei@gmail.com>
2015-12-31 15:37:57 +0800

    Update display test

--- scoap-web/routes/index.js
+++ scoap-web/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'SCoAP Demo' });
+  res.render('index', { title: '受限应用 Web 管理界面' });
 });
 
 module.exports = router;
